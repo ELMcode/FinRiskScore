13a55149e8a9678254eb390d97d249d64b91f81d
elm <elm.dev.code@gmail.com>
2023-08-20 22:06:12 +0200

    feat (frontend): Minor adjustments + Added comment script chore (backend): Added comment script

--- backend/api/handlers/calculate_risk_score_handler.go
+++ backend/api/handlers/calculate_risk_score_handler.go
@@ -1,4 +1,4 @@
-// calculate_risk_score_handler.go
+// Package handlers calculate_risk_score_handler.go
 package handlers
 
 import (
@@ -11,6 +11,7 @@ import (
 	"FinRiskScore/backend/data"
 )
 
+// Response Structure de réponse pour endpoint CalculateRiskScoreHandler
 type Response struct {
 	Symbol    string  `json:"symbol"`
 	RiskScore float64 `json:"riskScore"`
@@ -27,39 +28,48 @@ type Response struct {
 	IsActivelyTrading bool    `json:"isActivelyTrading"`
 }
 
+// CalculateRiskScoreHandler gère la demande de calcul du score de risque financier
 func CalculateRiskScoreHandler(w http.ResponseWriter, r *http.Request) {
 
+	// Récupére la clé API à partir du dossier/fichier config
 	apiKey := config.APIKey
 
+	// Extraction du symbole de l'URL
 	symbol := r.URL.Query().Get("symbol")
 
+	// Valide les paramètres de calcul du score de risque financier
 	params := validators.CalculateRiskScoreParams{Symbol: symbol}
-
 	if err := validators.ValidateCalculateRiskScoreParams(params); err != nil {
 		http.Error(w, "Invalid params", http.StatusBadRequest)
 		return
 	}
 
+	// Récup les données financières à partir de l'API externe
 	financialData, err := data.FetchFinancialData(apiKey, symbol)
 	if err != nil {
 		http.Error(w, "Error fetching data", http.StatusInternalServerError)
 		return
 	}
 
+	// Vérifie les données financières récupérées
 	if len(financialData) == 0 {
 		http.Error(w, "No data found", http.StatusNotFound)
 		return
 	}
 
+	// Utilise le calcul du score de risque financier du dossier algorithm
 	riskScore := algorithm.CalculateRiskScore(financialData)
+	// limite à 2 décimales le résultat du risque financier
 	formattedRiskScore := float64(int(riskScore*100)) / 100
 
+	// Récupére les données de description de l'entreprise de l'api externe de description
 	description, err := data.FetchCompanyDescription(apiKey, symbol)
 	if err != nil {
 		http.Error(w, "Error fetching description", http.StatusInternalServerError)
 		return
 	}
 
+	// Création de la réponse JSON
 	resp := Response{
 		Symbol:    symbol,
 		RiskScore: formattedRiskScore,
@@ -76,14 +86,14 @@ func CalculateRiskScoreHandler(w http.ResponseWriter, r *http.Request) {
 		IsActivelyTrading: description[0].IsActivelyTrading,
 	}
 
+	// Encodage de la réponse en JSON
 	jsonResp, err := json.Marshal(resp)
-
 	if err != nil {
 		http.Error(w, "Error encoding response", http.StatusInternalServerError)
 		return
 	}
 
+	// Définition de l'en-tête header de la réponse JSON
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonResp)
-
 }
--- frontend/frontend-app/src/views/CalculateRiskScore.vue
+++ frontend/frontend-app/src/views/CalculateRiskScore.vue
@@ -1,15 +1,18 @@
 <template>
-  <v-row>
-
+  <!-- Barre de recherche -->
+  <v-row class="d-flex flex-wrap">
     <v-col cols="6">
+
       <v-text-field
           clearable
           label="Search stock symbol"
           v-model="symbol"
           @keyup.enter="search"
       />
+     <!--  v-model="symbol" -> lie le champ de texte à la variable reactive 'symbol' -->
     </v-col>
 
+    <!-- Bouton vers lien listes symbol  -->
     <v-col cols="6">
       <v-btn
           variant="tonal"
@@ -20,26 +23,23 @@
         Stock symbols list
       </v-btn>
     </v-col>
-
   </v-row>
 
-
+  <!-- Conteneur d'informations de l'entreprise -->
   <v-container class="wallboard" fluid>
-
-    <v-row>
-
+    <v-row class="d-flex flex-wrap">
+      <!-- Boucle sur les données de l'entreprise pour afficher les card -->
       <v-col
           v-for="(data, index) in companyData"
           :key="data.title"
           :cols="getCols(data.title)"
       >
-
         <v-card
             :color="cardColors[data.title]"
-            class="pa-0 white--text"
+            class="pa-2 white--text"
             style="height: 100%;"
         >
-
+          <!-- titre de la card sauf pour 'Image' et 'Description' -->
           <v-card-title
               class="text-uppercase text-subtitle-2 font-weight-bold"
               v-if="data.title !== 'Image' && data.title !== 'Description'"
@@ -47,38 +47,28 @@
             {{ data.title }}
           </v-card-title>
 
+          <!-- Contenu de la card -->
           <v-card-text class="d-flex justify-center text-h6">
-
+            <!-- Affichage en fonction du titre de la donnée -->
             <template v-if="data.title === 'Website'">
               <a :href="data.value" target="_blank">{{ data.value }}</a>
             </template>
-
             <template v-else-if="data.title === 'Image'">
-              <img :src="data.value" alt="Company Image" style="max-width: 20%;" />
+              <img :src="data.value" alt="Company Image" style="max-width: 25%;" />
             </template>
-
             <template v-else-if="data.title === 'Trading'">
-
-              <span :class="{online: data.value, offline: !data.value}">
+              <span>
                 {{ data.value ? 'Online' : 'Offline' }}
               </span>
-
             </template>
-
             <template v-else>
               {{ data.value }}
             </template>
-
           </v-card-text>
-
         </v-card>
-
       </v-col>
-
     </v-row>
-
   </v-container>
-
 </template>
 
 <script setup lang="ts">
@@ -87,6 +77,7 @@ import { useRoute } from 'vue-router';
 import { onMounted, ref } from "vue";
 import router from "@/router";
 
+// Données de l'entreprise
 const companyData = ref([
   { title: 'Company Name', value: '' },
   { title: 'Risk Score', value: 0 },
@@ -100,28 +91,15 @@ const companyData = ref([
   { title: 'Is Actively Trading', value: false }
 ]);
 
-const cardColors = {
-  'Company Name': 'blue',
-  'Financial Risk Score': 'red',
-  'Industry': 'green',
-  'Description': 'brown',
-  'Price': 'orange',
-  'Changes': 'pink',
-  'Website': 'teal',
-  'Image': 'grey',
-  'Exchange': 'cyan',
-  'Trading': 'amber'
-};
-
 const route = useRoute();
 
+// Au montage du composant
 onMounted(async () => {
   const symbol = route.query.symbol as string;
   const response = await fetch(`http://localhost:8080/calculate-risk-score?symbol=${symbol}`);
   const data = await response.json();
 
-  console.log(data);
-
+  // Mise à jour des données de l'entreprise
   companyData.value = [
     { title: 'Company Name', value: data.companyName },
     { title: 'Image', value: data.image },
@@ -133,40 +111,50 @@ onMounted(async () => {
     { title: 'Exchange', value: data.exchange },
     { title: 'Trading', value: data.isActivelyTrading },
     { title: 'Website', value: data.website },
-
   ];
 });
 
+// Fonction pour déterminer le nombre de colonnes en fonction du titre (utile pour 'Description')
 const getCols = (title: string) => {
   return title === 'Description' ? 12 : 4;
 };
 
+// crée une variable réactive 'symbol', utilisé pour récupérer la valeur du texte de la barre de recherche
 const symbol = ref('');
+// Fonction de recherche
 function search() {
+  // Redirige vers  nouvelle URL avec le chemin '/calculate-risk-score' + symbole saisi de la barre de recherche via la variable reactive 'symbol'
   router.push({
     path: '/calculate-risk-score',
     query: {
       symbol: symbol.value
     }
   });
+  // Rechargement de la page apres une modification de route (suite à la barre de recherche)
   router.afterEach(() => {
     location.reload();
   });
 }
+
+// Couleurs des card en fonction du titre
+const cardColors = {
+  'Company Name': '#5e94e3',
+  'Financial Risk Score': '#D9463F',
+  'Industry': '#caec6d',
+  'Description': '#b8cfe0',
+  'Price': '#f3c13e',
+  'Changes': '#9FC131',
+  'Website': '#92d0f5',
+  'Image': '#E6E6E6',
+  'Exchange': '#5FA1D4',
+  'Trading': '#81CF95'
+};
 </script>
 
 <style>
 .wallboard {
-  padding: 20px;
-  width: 90%;
-  background-color:#424242;
-}
-
-.online {
-  margin-right: 5px;
-}
-
-.offline {
-  margin-right: 5px;
+  padding: 25px;
+  max-width: 80%;
+  background-color:#F4F8FB;
 }
 </style>
